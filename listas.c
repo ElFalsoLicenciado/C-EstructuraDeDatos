58666fe14030825f30976a8503dd4e837a4d3c7a
ElFalsoLicendiado <chopytrollface@gmail.com>
2024-10-08 18:08:34 -0600

    Avance #1

---
+++ listas.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define M 5
+
+/* char *fgets_clean(char *buffer, int len)
+{
+    fgets(buffer,len,stdin);
+
+    if (strchr(buffer, '\n') == NULL)
+        while ((fgetc(stdin)) != '\n');
+
+        buffer[strcspn(buffer, "\n")] = '\0';
+
+        return buffer;
+}
+*/
+
+struct nodo
+{
+    int valor;
+    struct nodo *sig;
+};
+
+struct nodo *raiz = NULL;
+
+/* Listas:
+    Insertar elementos
+    Sacar elementos
+    Y insertado ordenado.
+*/
+
+int main()
+{
+
+    return 1;
+}
+
+insertar_ord(int x)
+{
+    struct nodo *aux,*aux2;
+
+    aux = (struct nodo*)malloc(sizeof(struct nodo));
+    aux->valor = x; aux->sig=NULL;
+
+    if(raiz == NULL)
+    {
+        raiz = aux;
+    }
+    else 
+    {
+        if(x<raiz->valor)
+        {
+            aux->sig = raiz->sig;
+            raiz = aux;
+        }else
+        {
+            aux2 = raiz;
+
+            while (aux2->valor < x && aux2->sig->valor < x && aux2->sig != NULL)
+            {
+                aux->sig = aux2->sig;
+                aux2->sig = aux;
+            }
+            
+        }
+
+    }
+}
+
+eliminar(int x)
+{
+    struct nodo *aux; aux = raiz;
+    
+    if (raiz->valor == x)
+    {
+        raiz = raiz->sig; free(aux);
+    }
+    else
+    {
+        while(aux->sig->valor != x && aux->sig != NULL)
+            aux = aux->sig;
+
+        if(aux->sig == NULL) printf("No existe el elemento %d",x);
+        else
+        {
+            aux->sig = aux->sig->sig;
+            aux = aux->sig;
+            free(aux);
+        }
+    }
+}
